dab36611f0c3c9f177fe67691af6786a1ef8c2a5
Adrian Metlenko <a.metlenko@garvan.org.au>
2024-09-30 11:09:00 +1000

    Adding semantic HTML tags

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div>
+            <Nav/>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
